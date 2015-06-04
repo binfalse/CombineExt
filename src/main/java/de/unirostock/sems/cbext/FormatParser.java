0def00128bd3c43e992a70bd9a772c4a0e22529f
mp487 <martin.peters3@uni-rostock.de>
2015-06-04 12:23:53 +0200

    added format parser interface

---
+++ src/main/java/de/unirostock/sems/cbext/FormatParser.java
@@ -0,0 +1,28 @@
+package de.unirostock.sems.cbext;
+
+import java.io.File;
+import java.net.URI;
+
+public interface FormatParser {
+	
+	/**
+	 * Defines the priority of this format parser.
+	 * The higher the priority, the sooner this parser gets called. <br>
+	 * 
+	 * Should not be negative. Default format parser have priority around 100.
+	 * 
+	 * @return an integer > 0
+	 */
+	public int getPriority();
+	
+	/**
+	 * Parses the given file and tries to determine an identifying URL, like purl.org.
+	 * If this is not possible, null should be returned.
+	 * 
+	 * @param file Path to the file
+	 * @param mimeType MIME type for quick evaluation.
+	 * @return A formate URL or null.
+	 */
+	public URI checkFormat(File file, String mimeType);
+	
+}
